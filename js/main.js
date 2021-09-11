32c45a4aea46915bac1473b27ccd0f0050772c8b
KirpaOleg <profix.ok@gmail.com>
2021-09-11 12:26:37 +0300

    hw13

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let a = 100;
+let d= 10;
+let c = 5;
+
+let x = (d-a)/(c*2);
+console.log(x);
+
+
+let q = 20;
+let w = 20;
+let e = 4;
+let z = w * (e-1);
+let m = ((((q*2)+(w*2))*e)-z); 
+console.log(m);
